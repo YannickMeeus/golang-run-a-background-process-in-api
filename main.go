6802a51158ce64a9770d0773695e6801ef7c582b
Yannick Meeus <yannick.meeus@gmail.com>
2021-10-18 15:51:13 +0300

    Add simple go routine saying hi every 5 seconds :flex:

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	go sayHiEvery5Seconds()
+
 	port := "8000"
 	http.HandleFunc("/_ping", ping)
-	http.HandleFunc("/guess-the-age", handleGuessTheAge)
 	log.Printf("Server started at port %v", port)
 	log.Fatal(http.ListenAndServe("localhost:" + port, nil))
+}
+
+func sayHiEvery5Seconds() {
+	for {
+		fmt.Println("Hello! ð")
+		time.Sleep(5 * time.Second)
+	}
 }
