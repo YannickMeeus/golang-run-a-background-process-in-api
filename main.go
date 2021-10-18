32fadf443f0764f47ef178c1e5d30bbfcce682c3
Yannick Meeus <yannick.meeus@gmail.com>
2021-10-18 15:28:30 +0300

    Extract method to make core of the post handling a bit more readable

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	port := "8000"
 	http.HandleFunc("/_ping", ping)
-	http.HandleFunc("/guess-the-age", handleGuessTheAgePost)
+	http.HandleFunc("/guess-the-age", handleGuessTheAge)
 	log.Printf("Server started at port %v", port)
 	log.Fatal(http.ListenAndServe("localhost:" + port, nil))
 }
